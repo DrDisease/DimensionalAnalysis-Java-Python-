c78a20c176add066e826705c7f636dd7605e0b29
Rafael Santos <32169007+DrDisease@users.noreply.github.com>
2018-06-27 22:13:54 +0100

    1st day progress
    Now allows all the simpe algebric operations. Needs more code commented and to fix some problems with the code.
    Still needs testing

---
+++ AlgebricManipulation.java
@@ -0,0 +1,38 @@
+//import java.util.Scanner;
+
+public class AlgebricManipulation {
+
+	//Scanner sc = new Scanner(System.in);
+	
+	public class Variable{					// Support Type for algebric manipulation		
+		private Unit un;
+		private double value;
+		public Unit getUnit() {return un;}
+		public double getValue() {return value;}
+		public Variable(Unit n, double v) {un=n;value=v;}
+		}
+		public Variable var_mult(Variable v1,Variable v2) {				// Multiplication of this*v1
+			double new_value = v1.value * v2.value;
+			Unit res = v1.un.Unit_Mult(v2.un);
+			//res.setName(sc.next)
+			return new Variable(res, new_value);
+		}
+		public Variable var_div(Variable v1,Variable v2) {				// Division of this/v1
+			double new_value = v1.value / v2.value;
+			Unit res = v1.un.Unit_Div(v2.un);
+			//res.setName(sc.next)
+			return new Variable(res, new_value);
+		}
+		public Variable var_add(Variable v1, Variable v2) {				//Adiition of this+v1
+			if(!(v1.un.getName().equals(v2.getUnit().getName()))) {System.err.println("TYPE_MISMATCH");}
+			double new_value = v1.value + v2.value;
+			return new Variable(v1.un,new_value);
+		}
+		public Variable var_sub(Variable v1,Variable v2) {				// Subtraction of this-v1
+			if(!(v1.un.getName().equals(v2.un.getName()))) {System.err.println("TYPE_MISMATCH");}
+			double new_value = v1.value - v2.getValue();
+			return new Variable(v1.un,new_value);
+		}
+		
+	}
+
--- Unit.java
+++ Unit.java
@@ -1,12 +1,13 @@
 import java.util.LinkedList;
 import java.util.HashMap;
+import java.util.Iterator;
 
 	public class Unit{
 		private class tmp_unit_exp{
 			String uni;
 			int deg;
 			private tmp_unit_exp(String u,int e) {uni=u;deg=e;}
-		}
+			}
 		
 		private static java.util.HashMap<String,String> UnitList = new HashMap<String,String>(); 
 		private String name;
@@ -14,17 +15,126 @@ import java.util.HashMap;
 		public Unit(String name, String formula) {this.name=name;this.formula=formula;UnitList.put(name, formula);}
 		public String getName() {return name;}
 		public String getFormula() {return formula;}
-		public Unit Unit_Mult(Unit n) {
-			LinkedList<tmp_unit_exp> tmp = new LinkedList<tmp_unit_exp>();
-			String s1 = n.getFormula();
-			String s2 = this.formula;
-			//Code to parse the units and degree to the linked list
-			return new Unit("","");
-		}
-		
 		
 		public static boolean CheckList_name(String name) {
 			if (UnitList.containsKey(name)){return true;}
 			return false;
 			}
+		
+		
+		
+		public void setName(String n) {this.name=n;}
+		
+		//Formulas must come in Strings (s(^INT)?('*'(^INT)?)*)
+		public Unit Unit_Mult(Unit n) {
+			LinkedList<tmp_unit_exp> tmp = new LinkedList<tmp_unit_exp>();
+			String s1 = n.getFormula();
+			String s2 = this.formula;
+			//Code to parse the units and degree to the linked list
+			
+			String[] s1_units = s1.split("[*]");
+			String[] s2_units = s2.split("[*]");
+			int i ;
+			Iterator<tmp_unit_exp> iter = tmp.iterator();
+			String tmp_str;
+			for(String s : s1_units) {
+				if(s.contains("^^")) {
+					tmp_str =s.substring(0, s.indexOf("^")) ;
+					i = Integer.parseInt(s.substring(s.indexOf("^")+1));
+				}
+				else {
+					tmp_str = s;
+					i = 1;
+				}
+					tmp_unit_exp tmp_var = null;
+					boolean control = false;
+					while(iter.hasNext()) {
+						tmp_var = iter.next();
+						if(tmp_var.uni.equals(tmp_str)) {control = true;break;}
+					}
+					if(control) {tmp_var.deg+=i;}
+					else {tmp.add(new tmp_unit_exp("s",i));}
+				
+			}
+			for(String s : s2_units) {
+				if(s.contains("^^")) {
+					tmp_str =s.substring(0, s.indexOf("^")) ;
+					i = Integer.parseInt(s.substring(s.indexOf("^")+1));
+				}
+				else {
+					tmp_str = s;
+					i = 1;
+				}
+					tmp_unit_exp tmp_var = null;
+					boolean control = false;
+					while(iter.hasNext()) {
+						tmp_var = iter.next();
+						if(tmp_var.uni.equals(tmp_str)) {control = true;break;}
+					}
+					if(control) {tmp_var.deg+=i;}
+					else {tmp.add(new tmp_unit_exp("s",i));}
+			}
+			String result_final="";
+			for(tmp_unit_exp t : tmp) {
+			if(t.deg != 0) {result_final += t.uni+"^"+t.deg+"*";}
+			}
+			result_final = result_final.substring(0,result_final.length()-1);
+			return new Unit("UNDEFINED_VAR","result_final");
+		}
+		
+		public Unit Unit_Div(Unit n) {
+			LinkedList<tmp_unit_exp> tmp = new LinkedList<tmp_unit_exp>();
+			String s1 = n.getFormula();
+			String s2 = this.formula;
+			//Code to parse the units and degree to the linked list
+			
+			String[] s1_units = s1.split("[*]");
+			String[] s2_units = s2.split("[*]");
+			int i ;
+			Iterator<tmp_unit_exp> iter = tmp.iterator();
+			String tmp_str;
+			for(String s : s1_units) {
+				if(s.contains("^^")) {
+					tmp_str =s.substring(0, s.indexOf("^")) ;
+					i = Integer.parseInt(s.substring(s.indexOf("^")+1));
+				}
+				else {
+					tmp_str = s;
+					i = 1;
+				}
+					tmp_unit_exp tmp_var = null;
+					boolean control = false;
+					while(iter.hasNext()) {
+						tmp_var = iter.next();
+						if(tmp_var.uni.equals(tmp_str)) {control = true;break;}
+					}
+					if(control) {tmp_var.deg-=i;}
+					else {tmp.add(new tmp_unit_exp("s",i));}
+				
+			}
+			for(String s : s2_units) {
+				if(s.contains("^^")) {
+					tmp_str =s.substring(0, s.indexOf("^")) ;
+					i = Integer.parseInt(s.substring(s.indexOf("^")+1));
+				}
+				else {
+					tmp_str = s;
+					i = 1;
+				}
+					tmp_unit_exp tmp_var = null;
+					boolean control = false;
+					while(iter.hasNext()) {
+						tmp_var = iter.next();
+						if(tmp_var.uni.equals(tmp_str)) {control = true;break;}
+					}
+					if(control) {tmp_var.deg-=i;}
+					else {tmp.add(new tmp_unit_exp("s",i));}
+			}
+			String result_final="";
+			for(tmp_unit_exp t : tmp) {
+				if(t.deg != 0) {result_final += t.uni+"^"+t.deg+"*";}
+			}
+			result_final = result_final.substring(0,result_final.length()-1);
+			return new Unit("UNDEFINED_VAR","result_final");
+		}
 	}
